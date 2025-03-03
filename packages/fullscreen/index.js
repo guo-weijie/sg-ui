12967f2b284e534defbffcc40c7a91b783006dbb
guo-weijie <weijie.g@outlook.com>
2025-03-03 09:23:59 +0800

    移除fullscreen组件。原因：ruoyi框架已包含全屏组件

--- packages/fullscreen/index.js
+++
@@ -1,8 +0,0 @@
-import Fullscreen from './src/main';
-
-/* istanbul ignore next */
-Fullscreen.install = function(Vue) {
-  Vue.component(Fullscreen.name, Fullscreen);
-};
-
-export default Fullscreen;
