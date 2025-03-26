e50e0434b8e58ff53b57742d965a5c0b94d27d8a
chengrui2 <1435702715@qq.com>
2025-03-26 15:16:51 +0800

    sg-ui新规范修改提交

--- packages/transfer/src/main.vue
+++ packages/transfer/src/main.vue
@@ -1,20 +1,20 @@
 <template>
-  <div class="sg-transfer">
+  <div class="sg-transfer" :class="this.buttonDirection === 'vertical'?'verticalTransfer':''">
     <transfer-panel v-bind="$props" ref="leftPanel" :data="sourceData" :title="titles[0] || t('el.transfer.titles.0')"
       :default-checked="leftDefaultChecked" :placeholder="filterPlaceholder || t('el.transfer.filterPlaceholder')"
-      @checked-change="onSourceCheckedChange">
+      @checked-change="onSourceCheckedChange" >
       <slot name="left-footer"></slot>
     </transfer-panel>
-    <div class="sg-transfer__buttons">
+    <div class="sg-transfer__buttons" :class="this.buttonDirection === 'vertical'?'verticalTransferBtn':''">
       <sg-button type="primary" :class="['sg-transfer__button', hasButtonTexts ? 'is-with-texts' : '']"
         @click.native="addToLeft" :disabled="rightChecked.length === 0">
-        <i class="sg-icon-arrow-left"></i>
+        <i :class="buttonIcons[0]"></i>
         <span v-if="buttonTexts[0] !== undefined">{{ buttonTexts[0] }}</span>
       </sg-button>
       <sg-button type="primary" :class="['sg-transfer__button', hasButtonTexts ? 'is-with-texts' : '']"
         @click.native="addToRight" :disabled="leftChecked.length === 0">
         <span v-if="buttonTexts[1] !== undefined">{{ buttonTexts[1] }}</span>
-        <i class="sg-icon-arrow-right"></i>
+        <i :class="buttonIcons[1]"></i>
       </sg-button>
     </div>
     <transfer-panel v-bind="$props" ref="rightPanel" :data="targetData" :title="titles[1] || t('el.transfer.titles.1')"
@@ -105,7 +105,19 @@ export default {
     targetOrder: {
       type: String,
       default: 'original'
-    }
+    },
+    buttonDirection:{
+      type: String,
+      default: 'horizontal'
+    },
+    buttonIcons:{
+      type: Array,
+      default() {
+        return ['sg-icon-arrow-left','sg-icon-arrow-right'];
+      }
+    },
+   
+
   },
 
   data() {
@@ -141,7 +153,7 @@ export default {
 
     hasButtonTexts() {
       return this.buttonTexts.length === 2;
-    }
+    },
   },
 
   watch: {
